a80b05febec6e33b4ec950a05f8d1b070db03af9
sl846 <shiyu.liu2@duke.edu>
2024-03-27 12:03:24 -0400

    add test for orm extra credit

--- project4/exerciser.cpp
+++ project4/exerciser.cpp
@@ -1,10 +1,10 @@
 #include "exerciser.h"
 
 void exercise(connection * C) {
-  add_state(C, "state1");
-  add_color(C, "color1");
-  add_team(C, "team1", 1, 1, 0, 0);
-  add_player(C, 1, 1, "play", "er1", 1, 0, 0, 0, 0, 0);
+  // add_state(C, "state1");
+  // add_color(C, "color1");
+  // add_team(C, "team1", 1, 1, 0, 0);
+  // add_player(C, 1, 1, "play", "er1", 1, 0, 0, 0, 0, 0);
   query1(C, 1, 35, 40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
   query2(C, "LightBlue");
   query2(C, "Maroon");
--- project4/proj4_extra_sl846/main.py
+++ project4/proj4_extra_sl846/main.py
@@ -2,19 +2,27 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
-from query import query1,query2,query3,query4,query5 
+from query import query1, query2, query3, query4, query5
 
 DATABASE_URL = "postgresql://postgres:passw0rd@localhost/ACC_BBALL"
 
+
 def main():
     engine = create_engine(DATABASE_URL)
-    Base.metadata.create_all(engine)  
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    query2(session, "Orange")
+    query1(session, 1, 35, 40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    query1(session, 1, 35, 40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0)
+    query2(session, "LightBlue")
+    query2(session, "Maroon")
     query3(session, "Duke")
-
+    query3(session, "UNC")
+    query4(session, "NC", "DarkBlue")
+    query5(session, 10)
+    query5(session, 13)
     session.close()
+
 
 if __name__ == "__main__":
     main()
